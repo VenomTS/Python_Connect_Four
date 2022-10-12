f4a3e06ce7859ae6e0a8cad4ec831302cba113aa
VenomTS <thefrostlord10@gmail.com>
2022-10-12 07:05:08 +0200

    Add files via upload

--- board.py
+++ board.py
@@ -1,12 +1,15 @@
 import math
 
+from PIL import ImageGrab
+
 import pyautogui
 
-
 class Spot:
-    def __init__(self, row, col, color):
-        self.row = col
-        self.col = row
+    def __init__(self, row, col, color, startX, startY, distance):
+        self.row = row
+        self.col = col
+        self.posX = (col * distance) + startX
+        self.posY = (row * distance) + startY
         self.color = color
 
 class Board:
@@ -15,6 +18,8 @@ class Board:
         self.NEUTRAL = (16, 27, 39)
         self.FRIENDLY = (24, 188, 156)
         self.ENEMY = (238, 102, 119)
+
+        self.MOVES = 7
 
         self.rows = rows
         self.cols = cols
@@ -31,24 +36,47 @@ class Board:
         for i in range(self.rows):
             b.append([])
             for j in range(self.cols):
-                b[i].append(Spot(i, j, self.NEUTRAL))
+                b[i].append(Spot(i, j, self.NEUTRAL, self.startX, self.startY, self.distance))
         return b
 
+    def getColor(self, color):
+        if color == self.FRIENDLY:
+            return "F"
+        elif color == self.ENEMY:
+            return "E"
+        return "N"
+
+    def createTransposition(self):
+        lastColor = self.board[0][0].color
+        colorCount = 1
+        transposition = ""
+        for row in range(self.rows):
+            for col in range(self.cols):
+                if row == col == 0:
+                    continue
+                currSpot = self.board[row][col]
+                if lastColor == currSpot.color:
+                    colorCount += 1
+                else:
+                    if colorCount > 1:
+                        transposition += str(colorCount)
+                    self.getColor(lastColor)
+                    lastColor = currSpot.color
+                    colorCount = 1
+        if colorCount > 1:
+            transposition += str(colorCount)
+        transposition += self.getColor(lastColor)
+        return transposition
+
+
     def updateBoard(self):
-        """
-
-        Scans the board on site and matches it with the board in the code
-        If number of enemy positions has increased, allows itself to make a move
-
-        """
+        img = ImageGrab.grab(bbox=(0, 0, 1920, 1080))
         currEnemy = 0
         currFriendly = 0
         for row in range(self.rows):
             for col in range(self.cols):
                 currSpot = self.board[row][col]
-                currX = (currSpot.row * self.distance) + self.startX
-                currY = (currSpot.col * self.distance) + self.startY
-                pixelColor = pyautogui.pixel(currX, currY)
+                pixelColor = img.getpixel((currSpot.posX, currSpot.posY))
                 if currSpot.color != pixelColor:
                     currSpot.color = pixelColor
                 if pixelColor == self.ENEMY:
@@ -63,138 +91,136 @@ class Board:
     def findAvailableMoves(self):
         available = []
         for col in range(self.cols):
+            col = self.cols // 2 + (1 - 2 * (col % 2)) * (col + 1) // 2
             for row in range(self.rows - 1, -1, -1):
                 if self.board[row][col].color == self.NEUTRAL:
-                    available.append((row, col))
+                    available.append(self.board[row][col])
                     break
         return available
 
-    def calculatePosition(self):
-        scores = [0, 0, 0, 0]
-        for row in range(self.rows):  # Left to Right
-            for col in range(self.cols - 3):
-                fScore, eScore = 0, 0
-                for i in range(4):
-                    currSpotColor = self.board[row][col + i].color
-                    if currSpotColor == self.FRIENDLY:
-                        fScore += 100
-                        eScore = 0
-                    elif currSpotColor == self.ENEMY:
-                        fScore = 0
-                        eScore += 100
-                    else:
-                        fScore += 10
-                        eScore += 10
-                if eScore >= 400:
-                    return -math.inf
-                elif fScore >= 400:
-                    return math.inf
-                scores[0] += (fScore - eScore)
-        for col in range(self.cols):  # Up to Down
-            for row in range(self.rows - 3):
-                fScore, eScore = 0, 0
-                for i in range(4):
-                    currSpotColor = self.board[row + i][col].color
-                    if currSpotColor == self.FRIENDLY:
-                        fScore += 100
-                        eScore = 0
-                    elif currSpotColor == self.ENEMY:
-                        fScore = 0
-                        eScore += 100
-                    else:
-                        fScore += 10
-                        eScore += 10
-                if eScore >= 400:
-                    return -math.inf
-                elif fScore >= 400:
-                    return math.inf
-                scores[1] += (fScore - eScore)
-        for row in range(self.rows - 3):  # Left to Right Diagonal
-            for col in range(self.cols - 3):
-                fScore, eScore = 0, 0
-                for i in range(4):
-                    currSpotColor = self.board[row + i][col + i].color
-                    if currSpotColor == self.FRIENDLY:
-                        fScore += 100
-                        eScore = 0
-                    elif currSpotColor == self.ENEMY:
-                        fScore = 0
-                        eScore += 100
-                    else:
-                        fScore += 10
-                        eScore += 10
-                if eScore >= 400:
-                    return -math.inf
-                elif fScore >= 400:
-                    return math.inf
-                scores[2] += (fScore - eScore)
-        for row in range(self.rows - 3):  # Right to Left Diagonal
-            for col in range(self.cols - 1, self.cols - 5, -1):
-                fScore, eScore = 0, 0
-                for i in range(4):
-                    currSpotColor = self.board[row + i][col - i].color
-                    if currSpotColor == self.FRIENDLY:
-                        fScore += 100
-                        eScore = 0
-                    elif currSpotColor == self.ENEMY:
-                        fScore = 0
-                        eScore += 100
-                    else:
-                        fScore += 10
-                        eScore += 10
-                if eScore >= 400:
-                    return -math.inf
-                elif fScore >= 400:
-                    return math.inf
-                scores[3] += (fScore - eScore)
-        return sum(scores)
-
-    def gameEnded(self):
+    def isDraw(self):
         occ = 0
         for row in self.board:
-            for item in row:
-                if item.color != self.NEUTRAL:
+            for currSpot in row:
+                if currSpot.color != self.NEUTRAL:
                     occ += 1
-        return occ == (self.rows * self.cols)
+        return occ == (self.rows * self.cols), 0
 
-    def makeMove(self):  # Move[0] = X, Move[1] = Y; Switch because IDK...
-        score, move = self.minimax(6, True, -math.inf, math.inf, [])
-        currX = (move[1] * self.distance) + self.startX
-        currY = (move[0] * self.distance) + self.startY
+    def gameEnded(self):
+        b = self.board
+        for row in range(self.rows):
+            for col in range(self.cols - 3):
+                if b[row][col].color == b[row][col + 1].color == b[row][col + 2].color == b[row][col + 3].color and b[row][col].color != self.NEUTRAL:
+                    return (True, 123_456) if b[row][col].color == self.FRIENDLY else (True, -123_456)
+        for col in range(self.cols):
+            for row in range(self.rows - 3):
+                if b[row][col].color == b[row + 1][col].color == b[row + 2][col].color == b[row + 3][col].color and b[row][col].color != self.NEUTRAL:
+                    return (True, 123_456) if b[row][col].color == self.FRIENDLY else (True, -123_456)
+        for row in range(self.rows - 3):
+            for col in range(self.cols - 3):
+                if b[row][col].color == b[row + 1][col + 1].color == b[row + 2][col + 2].color == b[row + 3][col + 3].color and b[row][col].color != self.NEUTRAL:
+                    return (True, 123_456) if b[row][col].color == self.FRIENDLY else (True, -123_456)
+        for row in range(self.rows - 3):  # Right to Left Diagonal
+            for col in range(self.cols - 1, self.cols - 5, -1):
+                if b[row][col].color == b[row + 1][col - 1].color == b[row + 2][col - 2].color == b[row + 3][col - 3].color and b[row][col].color != self.NEUTRAL:
+                    return (True, 123_456) if b[row][col].color == self.FRIENDLY else (True, -123_456)
+        return self.isDraw()
+
+    def calculatePosition(self):
+        friendlyConnections = [0, 0, 0, 0, 0]
+        enemyConnections = [0, 0, 0, 0, 0]
+        for row in range(self.rows):  # Left to Right
+            for col in range(self.cols - 3):
+                colors = []
+                for i in range(4):
+                    colors.append(self.board[row][col + i].color)
+                friendlyScore = colors.count(self.FRIENDLY)
+                enemyScore = colors.count(self.ENEMY)
+                neutralScore = colors.count(self.NEUTRAL)
+                if friendlyScore >= 1 and neutralScore + friendlyScore == 4:
+                    friendlyConnections[friendlyScore] += 1
+                if enemyScore >= 1 and neutralScore + enemyScore == 4:
+                    enemyConnections[enemyScore] += 1
+        for col in range(self.cols):  # Up to Down
+            for row in range(self.rows - 3):
+                colors = []
+                for i in range(4):
+                    colors.append(self.board[row + i][col].color)
+                friendlyScore = colors.count(self.FRIENDLY)
+                enemyScore = colors.count(self.ENEMY)
+                neutralScore = colors.count(self.NEUTRAL)
+                if friendlyScore >= 1 and neutralScore + friendlyScore == 4:
+                    friendlyConnections[friendlyScore] += 1
+                if enemyScore >= 1 and neutralScore + enemyScore == 4:
+                    enemyConnections[enemyScore] += 1
+        for row in range(self.rows - 3):  # Left to Right Diagonal
+            for col in range(self.cols - 3):
+                colors = []
+                for i in range(4):
+                    colors.append(self.board[row + i][col + i].color)
+                friendlyScore = colors.count(self.FRIENDLY)
+                enemyScore = colors.count(self.ENEMY)
+                neutralScore = colors.count(self.NEUTRAL)
+                if friendlyScore >= 1 and neutralScore + friendlyScore == 4:
+                    friendlyConnections[friendlyScore] += 1
+                if enemyScore >= 1 and neutralScore + enemyScore == 4:
+                    enemyConnections[enemyScore] += 1
+        for row in range(self.rows - 3):  # Right to Left Diagonal
+            for col in range(self.cols - 1, self.cols - 5, -1):
+                colors = []
+                for i in range(4):
+                    colors.append(self.board[row + i][col - i].color)
+                friendlyScore = colors.count(self.FRIENDLY)
+                enemyScore = colors.count(self.ENEMY)
+                neutralScore = colors.count(self.NEUTRAL)
+                if friendlyScore >= 1 and neutralScore + friendlyScore == 4:
+                    friendlyConnections[friendlyScore] += 1
+                if enemyScore >= 1 and neutralScore + enemyScore == 4:
+                    enemyConnections[enemyScore] += 1
+        friendlySum = friendlyConnections[1] * 10 + friendlyConnections[2] * 100 + friendlyConnections[3] * 1000
+        enemySum = enemyConnections[1] * 10 + enemyConnections[2] * 100 + enemyConnections[3] * 1000
+        return friendlySum - enemySum
+
+    def makeMove(self):
+        currScore = -math.inf
+        spotToPlay = None
+        for currSpot in self.findAvailableMoves():
+            currSpot.color = self.FRIENDLY
+            score = self.minimax(self.MOVES - 1, False, -math.inf, math.inf)
+            currSpot.color = self.NEUTRAL
+            if score > currScore:
+                currScore = score
+                spotToPlay = currSpot
+        currX = spotToPlay.posX
+        currY = spotToPlay.posY
         pyautogui.moveTo(currX, currY)
-        print(score, move[0], move[1])
         pyautogui.leftClick()
         self.move = False
         pyautogui.moveTo(self.startX - self.distance, self.startY - self.distance)
 
-    def minimax(self, moves, turn, alpha, beta, positions):
-        if moves == 0 or self.gameEnded():
-            return self.calculatePosition(), (-1, -1)
-        if turn:  # True = AI moves, False = Enemy Moves
+    def minimax(self, moves, maximising, alpha, beta):
+        ended, score = self.gameEnded()
+        if ended:
+            return score
+        elif moves == 0:
+            return self.calculatePosition()
+        if maximising:  # True = AI moves, False = Enemy Moves
             bestScore = -math.inf
-            bestMove = (-1, -1)
-            availableMoves = self.findAvailableMoves()
-            for move in availableMoves:
-                row, col = move
-                currSpot = self.board[row][col]
-                currSpot.color = self.FRIENDLY
-                score = self.minimax(moves - 1, False, alpha, beta, positions)[0]
-                currSpot.color = self.NEUTRAL
-                if score >= bestScore:
-                    bestScore = score
-                    bestMove = (row, col)
-            return bestScore, bestMove
+            for availableSpot in self.findAvailableMoves():
+                availableSpot.color = self.FRIENDLY
+                bestScore = max(bestScore, self.minimax(moves - 1, False, alpha, beta))
+                availableSpot.color = self.NEUTRAL
+                if bestScore >= beta:
+                    break
+                alpha = max(alpha, bestScore)
+            return bestScore
         else:
             bestScore = math.inf
-            bestMove = (-1, -1)
-            availableMoves = self.findAvailableMoves()
-            for move in availableMoves:
-                row, col = move
-                currSpot = self.board[row][col]
-                currSpot.color = self.ENEMY
-                score = self.minimax(moves - 1, True, alpha, beta, positions)[0]
-                currSpot.color = self.NEUTRAL
-                if score <= bestScore:
-                    bestScore = score
-                    bestMove = (row, col)
-            return bestScore, bestMove
+            for availableSpot in self.findAvailableMoves():
+                availableSpot.color = self.ENEMY
+                bestScore = min(bestScore, self.minimax(moves - 1, True, alpha, beta))
+                availableSpot.color = self.NEUTRAL
+                if bestScore <= alpha:
+                    break
+                beta = min(beta, bestScore)
+            return bestScore
--- main.py
+++ main.py
@@ -9,17 +9,20 @@ COLS = 7
 
 def gameOn(x, y):
     board = Board(ROWS, COLS, x, y)
-    while not board.gameEnded():
+    board.calculatePosition()
+    while not board.gameEnded()[0]:
         board.updateBoard()
         if board.move:
             board.makeMove()
-            time.sleep(3)
+            time.sleep(1)
 
 def main():
     input()
     x, y = pyautogui.position()
     pyautogui.moveTo(50, 50)
+    time.sleep(1)
     gameOn(x, y)
+
 main()
 
 
